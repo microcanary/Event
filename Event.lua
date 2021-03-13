692c3601a1cdb3be8d738c56d925b1de3a8f2408
Red Canary Studio <RedCanaryStudios@gmail.com>
2021-03-12 16:37:06 -0800

    Create Event.lua

---
+++ Event.lua
@@ -0,0 +1,56 @@
+local Event = {}
+
+Event.new = function()
+    local obj = {
+        
+        Items = {};
+        Queued = {};
+
+        Invoke = function(self, ...)
+            
+            for i, v in ipairs(self.Queued) do
+                coroutine.resume(v)
+                table.remove(self.Queued, i)
+            end
+
+            for _, v in pairs(self.Items) do
+                coroutine.wrap(v)(...)
+            end
+            
+        end;
+
+        Wait = function(self)
+            local thread = coroutine.running()
+            table.insert(self.Queued, thread)
+            coroutine.yield()
+        end;
+
+        Connect = function(self, f)
+            local Owner = self
+            local Connected = f
+
+            local Connection
+            Connection = {
+                Disconnect = function(self)
+                    Owner.Items[Connection] = nil
+                end
+            }
+
+            self.Items[Connection] = f
+
+            return Connection
+        end;
+        
+    }
+    
+    local event = newproxy(true)
+    
+    local mt = getmetatable(event)
+    
+    mt.__index = obj
+    
+    return event
+    
+end
+
+return Event
